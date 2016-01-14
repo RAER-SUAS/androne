c3f5ca21fe2408f2da6ee11fcf8c4562eb8b6e86
Chris Chapogas <cchapogas@gmail.com>
2016-01-14 11:10:53 -0500

    Initial commit

---
+++ app/src/main/java/com/raer/suas/suaspayload/CameraConnection.java
@@ -0,0 +1,15 @@
+package com.raer.suas.suaspayload;
+
+import android.app.LoaderManager;
+import android.hardware.camera2.*;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+/**
+ * Created by cchap on 1/8/2016.
+ */
+public class CameraConnection {
+//while (!LoaderCallbackInterface.SUCCESS)
+
+
+
+}
---
+++ app/src/main/java/com/raer/suas/suaspayload/NetInfo.java
@@ -0,0 +1,16 @@
+package com.raer.suas.suaspayload;
+import android.net.*;
+
+import java.net.InetAddress;
+
+
+/**
+ * Created by cchap on 1/8/2016.
+ */
+public class NetInfo {
+    String networkState = NetworkInfo.State.CONNECTED.toString();
+   // String ipv4Address = Inet4Address.getLocalHost().getHostName();
+    InetAddress ipv4Address = java.net.InetAddress.getLoopbackAddress();
+    String sipv4Address = ipv4Address.toString();
+
+}
